50498339c8a6df820e11f2414363f93411c6f4ac
Matthew Kelly <matthew.kelly@block.one>
2019-04-15 14:47:10 +0800

    Changed things

--- packages/doctor/src/doctor.ts
+++ packages/doctor/src/doctor.ts
@@ -1,17 +1,17 @@
-import Patient from '@ajay-sdk/patient';
+// import Patient from '@ajay-sdk/patient';
 
 export default class Doctor {
   constructor(){}
 
   yo(){
-    return "doctor says yo";
+    return "doctor says yo111";
   }
   sup(){
-    return "sup doc111";
+    return "sup doc111"; 
   }
 
-  getpatient(){
-    let p = new Patient();
-    p.get(2);
-  }
+  // getpatient(){
+  //   let p = new Patient();
+  //   p.get(2);
+  // }
 }
